4f1bd2c8f925c999d3e841ad67fd5d1f68fb8ba2
dingcm <875708765@qq.com>
2024-01-31 22:18:49 +0800

    [feature]: merge pr https://github.com/json-path/JsonPath/pull/532

---
+++ json-path-assert/src/test/java/com/jayway/jsonpath/matchers/helpers/StrictParsingConfiguration.java
@@ -0,0 +1,53 @@
+package com.jayway.jsonpath.matchers.helpers;
+
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.Option;
+import com.jayway.jsonpath.spi.json.JsonProvider;
+import com.jayway.jsonpath.spi.json.JsonSmartJsonProvider;
+import com.jayway.jsonpath.spi.mapper.JsonSmartMappingProvider;
+import com.jayway.jsonpath.spi.mapper.MappingProvider;
+import com.jayway.jsonpath.spi.transformer.TransformationProvider;
+import com.jayway.jsonpath.spi.transformer.TransformationSpec;
+import net.minidev.json.parser.JSONParser;
+
+import java.io.InputStream;
+import java.util.EnumSet;
+import java.util.Set;
+
+public class StrictParsingConfiguration implements Configuration.Defaults {
+
+    private final JsonProvider jsonProvider = new JsonSmartJsonProvider(JSONParser.MODE_STRICTEST);
+    private final MappingProvider mappingProvider = new JsonSmartMappingProvider();
+
+    public JsonProvider jsonProvider() {
+        return jsonProvider;
+    }
+
+    public MappingProvider mappingProvider() {
+        return mappingProvider;
+    }
+
+    public Set<Option> options() {
+        return EnumSet.noneOf(Option.class);
+    }
+
+    @Override
+    public TransformationProvider transformationProvider() {
+        return new TransformationProvider() {
+            @Override
+            public TransformationSpec spec(String input, Configuration configuration) {
+                return null;
+            }
+
+            @Override
+            public TransformationSpec spec(InputStream input, Configuration configuration) {
+                return null;
+            }
+
+            @Override
+            public Object transform(Object source, TransformationSpec spec, Configuration configuration) {
+                return null;
+            }
+        };
+    }
+}
